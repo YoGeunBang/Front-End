6f0d231f667df495a3f7af11052b43553f191201
jhwmac <jkr2505@naver.com>
2022-11-18 20:56:05 +0900

    FIX: baseURL 재수정

--- pages/api/customAxios.ts
+++ pages/api/customAxios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'https://yogeunbang.swygbro.com/' : process.env.NEXT_PUBLIC_BACKEND,
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://yogeunbang.swygbro.com/',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
