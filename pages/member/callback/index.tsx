0ca04bc9f6c2b99cde75c565bcd520255aa3e6ea
jhwmac <jkr2505@naver.com>
2023-03-01 00:23:47 +0900

    FEAT: 로딩화면 및 애니메이션 구현

--- pages/member/callback/index.tsx
+++ pages/member/callback/index.tsx
@@ -3,32 +3,18 @@ import { AppLayout } from 'components/layout';
 import { ReactElement, useEffect } from 'react';
 import styled from 'styled-components';
 import Router from 'next/router';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { saveTokenAction } from 'store/token';
+
 
 const Page: NextPageWithLayout = () => {
-  const dispatch = useDispatch();
-  const BACKEND_URL = 'https://ygb.server.swygbro.com/members';
-  
-  const postToken = async (_token: string) => {
-    await axios
-      .post(`https://cors-anywhere.herokuapp.com/${BACKEND_URL}`, {
-        accessToken: _token,
-      })
-      .then((res) => {
-        dispatch(saveTokenAction({token: res.data.accessToken}));
-      });
-  };
 
   const getToken = () => {
     const hash = Router.asPath.split('#')[1]; // 네이버 로그인을 통해 전달받은 hash 값
     if (hash) {
       const token = hash.split('=')[1].split('&')[0]; // token값 확인
-      postToken(token);
       Router.push({
-        pathname: '/',
-      });
+        pathname: '/member/loading',
+        query: { token: token },
+      },'/member/loading');
       // naverLogin.getLoginStatus((status: any) => {
       //   if (status) {
       //     // 로그인 상태 값이 있을 경우
@@ -43,12 +29,10 @@ const Page: NextPageWithLayout = () => {
     getToken();
   }, []);
 
-  return <MypageEl>콜백페이지 입니다만..?</MypageEl>;
+  return (<></>);
 };
 
-Page.getLayout = function getLayout(page: ReactElement) {
-  return <AppLayout>{page}</AppLayout>;
-};
+
 const MypageEl = styled.div`
   padding: 80px 12.5% 10% 12.5%;
   position: relative;
